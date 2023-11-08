444cd2922c437004e0317df96bc0add373a3140e
Guyven <guyven@pm.me>
2023-11-07 20:16:24 -0500

    initial changes works but mumble doesn't see updates

--- Linkpearl/Plugin.cs
+++ Linkpearl/Plugin.cs
@@ -4,53 +4,59 @@ using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
-using Dalamud.Game;
-using Dalamud.Game.ClientState;
 using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.Command;
-using Dalamud.Game.Gui;
 using Dalamud.IoC;
-using Dalamud.Logging;
 using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Client.Game.Control;
 
 namespace Linkpearl;
+
+public class Dalamud {
+    public static void Initialize(DalamudPluginInterface pluginInterface) => pluginInterface.Create<Dalamud>();
+
+    [PluginService, RequiredVersion("1.0")] public static IFramework Framework { get; private set; } = null!;
+    [PluginService, RequiredVersion("1.0")] public static IClientState ClientState { get; private set; } = null!;
+    [PluginService, RequiredVersion("1.0")] public static IChatGui ChatGui { get; private set; } = null!;
+    [PluginService, RequiredVersion("1.0")] public static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService, RequiredVersion("1.0")] public static ICondition Condition { get; private set; } = null!;
+    [PluginService, RequiredVersion("1.0")] public static IPluginLog PluginLog { get; private set; } = null!;
+}
 
 public sealed class Plugin : IDalamudPlugin {
     public string Name => "Linkpearl";
     public const string CommandName = "/pearldebug";
 
-    [PluginService] public static Framework Framework { get; set; } = null!;
-    [PluginService] public static ClientState ClientState { get; set; } = null!;
-    [PluginService] public static ChatGui ChatGui { get; set; } = null!;
-    [PluginService] public static CommandManager CommandManager { get; set; } = null!;
-    [PluginService] public static Condition Condition { get; set; } = null!;
-
     private readonly MemoryMappedFile? _memoryMappedFile;
     private readonly MemoryMappedViewAccessor? _memoryMappedViewAccessor;
     private uint _tickCount;
 
-    public Plugin() {
-        Framework.Update += this.FrameworkUpdate;
+    public Plugin(DalamudPluginInterface pluginInterface) {
+        Dalamud.Initialize(pluginInterface);
 
-        CommandManager.AddHandler(CommandName, new CommandInfo(this.CommandHandler) {
+        Dalamud.Framework.Update += this.FrameworkUpdate;
+
+        Dalamud.CommandManager.AddHandler(CommandName, new CommandInfo(this.CommandHandler) {
             HelpMessage = "Debug command for Linkpearl.",
             ShowInHelp = false
         });
+
+        // Dalamud.ChatGui.Print("this is a test to see if the plugin is even loading");
 
         try {
             this._memoryMappedFile = MemoryMappedFile.CreateOrOpen("MumbleLink", Marshal.SizeOf<MumbleAvatar>());
             this._memoryMappedViewAccessor = this._memoryMappedFile.CreateViewAccessor();
         } catch (Exception e) {
-            PluginLog.Error(e, "Failed to create memory mapped file");
-            ChatGui.PrintError(
+            Dalamud.PluginLog.Error(e, "Failed to create memory mapped file");
+            Dalamud.ChatGui.PrintError(
                 "[Linkpearl] Failed to connect to Mumble. Make sure Mumble is open and re-enable the plugin!");
         }
     }
 
     public void Dispose() {
-        Framework.Update -= this.FrameworkUpdate;
-        CommandManager.RemoveHandler(CommandName);
+        Dalamud.Framework.Update -= this.FrameworkUpdate;
+        Dalamud.CommandManager.RemoveHandler(CommandName);
         this._memoryMappedViewAccessor?.Dispose();
         this._memoryMappedFile?.Dispose();
     }
@@ -58,38 +64,38 @@ public sealed class Plugin : IDalamudPlugin {
 
     private void CommandHandler(string cmd, string args) {
         var mumbleAvatar = this.BuildAvatar();
-        PluginLog.Debug($"Avatar exists: {mumbleAvatar != null}");
+        Dalamud.PluginLog.Debug($"Avatar exists: {mumbleAvatar != null}");
         if (mumbleAvatar == null) return;
 
         var avatarPos = mumbleAvatar.Value.AvatarPosition;
-        PluginLog.Debug($"Avatar position: {avatarPos[0]}, {avatarPos[1]}, {avatarPos[2]}");
+        Dalamud.PluginLog.Debug($"Avatar position: {avatarPos[0]}, {avatarPos[1]}, {avatarPos[2]}");
         var avatarFront = mumbleAvatar.Value.AvatarFront;
-        PluginLog.Debug($"Avatar front: {avatarFront[0]}, {avatarFront[1]}, {avatarFront[2]}");
+        Dalamud.PluginLog.Debug($"Avatar front: {avatarFront[0]}, {avatarFront[1]}, {avatarFront[2]}");
         var avatarTop = mumbleAvatar.Value.AvatarTop;
-        PluginLog.Debug($"Avatar top: {avatarTop[0]}, {avatarTop[1]}, {avatarTop[2]}");
+        Dalamud.PluginLog.Debug($"Avatar top: {avatarTop[0]}, {avatarTop[1]}, {avatarTop[2]}");
 
         var cameraPos = mumbleAvatar.Value.CameraPosition;
-        PluginLog.Debug($"Camera position: {cameraPos[0]}, {cameraPos[1]}, {cameraPos[2]}");
+        Dalamud.PluginLog.Debug($"Camera position: {cameraPos[0]}, {cameraPos[1]}, {cameraPos[2]}");
         var cameraFront = mumbleAvatar.Value.CameraFront;
-        PluginLog.Debug($"Camera front: {cameraFront[0]}, {cameraFront[1]}, {cameraFront[2]}");
+        Dalamud.PluginLog.Debug($"Camera front: {cameraFront[0]}, {cameraFront[1]}, {cameraFront[2]}");
         var cameraTop = mumbleAvatar.Value.CameraTop;
-        PluginLog.Debug($"Camera top: {cameraTop[0]}, {cameraTop[1]}, {cameraTop[2]}");
+        Dalamud.PluginLog.Debug($"Camera top: {cameraTop[0]}, {cameraTop[1]}, {cameraTop[2]}");
 
         var context = mumbleAvatar.Value.Context;
-        PluginLog.Debug($"Context: {Encoding.UTF8.GetString(context)}");
+        Dalamud.PluginLog.Debug($"Context: {Encoding.UTF8.GetString(context)}");
     }
 
     private unsafe MumbleAvatar? BuildAvatar() {
-        if (ClientState.LocalPlayer == null) return null;
+        if (Dalamud.ClientState.LocalPlayer == null) return null;
         if (this._memoryMappedFile == null) return null;
         if (this._memoryMappedViewAccessor == null) return null;
 
-        var avatarPos = ClientState.LocalPlayer.Position;
-        var avatarRot = ClientState.LocalPlayer.Rotation; // -pi to pi radians
+        var avatarPos = Dalamud.ClientState.LocalPlayer.Position;
+        var avatarRot = Dalamud.ClientState.LocalPlayer.Rotation; // -pi to pi radians
         var avatarFront = new Vector3((float)Math.Cos(avatarRot), 0, (float)Math.Sin(avatarRot));
         var avatarTop = new Vector3(0, 1, 0);
 
-        var camera = CameraManager.Instance->GetActiveCamera();
+        var camera = CameraManager.Instance()->GetActiveCamera();
         if (camera == null) return null;
 
         var cameraPos = camera->CameraBase.SceneCamera.Object.Position;
@@ -97,20 +103,20 @@ public sealed class Plugin : IDalamudPlugin {
         var cameraFront = new Vector3(cameraViewMatrix.M13, cameraViewMatrix.M23, cameraViewMatrix.M33);
         var cameraTop = camera->CameraBase.SceneCamera.Vector_1;
 
-        var contextId = ClientState.LocalPlayer.CurrentWorld.Id.ToString();
-        var boundByDuty = Condition[ConditionFlag.BoundByDuty]
-                          || Condition[ConditionFlag.BoundByDuty56]
-                          || Condition[ConditionFlag.BoundByDuty95];
+        var contextId = Dalamud.ClientState.LocalPlayer.CurrentWorld.Id.ToString();
+        var boundByDuty = Dalamud.Condition[ConditionFlag.BoundByDuty]
+                          || Dalamud.Condition[ConditionFlag.BoundByDuty56]
+                          || Dalamud.Condition[ConditionFlag.BoundByDuty95];
 
         if (boundByDuty) {
             contextId = "duty";
         }
 
-        var context = contextId + "-" + ClientState.TerritoryType;
+        var context = contextId + "-" + Dalamud.ClientState.TerritoryType;
         var contextBytes = new byte[256];
         var contextBytesWritten = Encoding.UTF8.GetBytes(context, contextBytes);
 
-        var cid = ClientState.LocalContentId.ToString("X8");
+        var cid = Dalamud.ClientState.LocalContentId.ToString("X8");
         var hash = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(cid));
         var identity = BitConverter.ToString(hash).Replace("-", "").ToLower();
 
@@ -163,7 +169,7 @@ public sealed class Plugin : IDalamudPlugin {
         };
     }
 
-    private void FrameworkUpdate(Framework onGodFr) {
+    private void FrameworkUpdate(IFramework onGodFr) {
         if (this._memoryMappedViewAccessor == null) return;
 
         var mumbleAvatar = this.BuildAvatar();
